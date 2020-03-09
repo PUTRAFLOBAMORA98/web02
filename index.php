b4c44afefa34e546add0ee53fbfa3e49cddb7cc7
PutraFobamora57 <PutraFlobamora98@gmail.com>
2020-03-09 19:57:41 +0700

    publish file index

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+
+<head>
+    <title>WEB02</title>
+</head>
+
+<body>
+
+    <?php
+    $A = 123;
+    function Test()
+    {
+        $A = "text";
+        echo "Nilai  dalam fungsi A = $A";
+    }
+    Test();
+    echo " NIlai dengan fungsi = $A \n";
+
+
+
+    ?>
+</body>
+
+
+</html>
