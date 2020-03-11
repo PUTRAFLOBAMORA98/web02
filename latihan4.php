1b35359e203180c188291a618f716b24bfdea185
PutraFobamora57 <PutraFlobamora98@gmail.com>
2020-03-11 13:58:51 +0700

    menghitung luas lingkaran

---
+++ latihan4.php
@@ -0,0 +1,23 @@
+<html>
+
+<head>
+    <title>WEB02</title>
+</head>
+
+<body>
+
+    <?php
+
+    use PhpParser\Node\Stmt\Echo_;
+
+    Define("judul", "Menghitung Luas Lingkaran");
+    define("phi", 3.14);
+    $r = 5;
+    $luas = phi * $r * $r;
+    echo judul;
+    echo "Luas = $luas";
+    ?>
+</body>
+
+
+</html>
