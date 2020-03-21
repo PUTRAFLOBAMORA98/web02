a5d82a2688ccd9b7ca9f2d7b73a9ad2a135ebf31
PutraFobamora57 <PutraFlobamora98@gmail.com>
2020-03-21 16:02:35 +0700

    03latihan php

---
+++ 03latihan1.php
@@ -0,0 +1,23 @@
+<?
+$ball = 'red';
+$semua = "yellow" . "blue" . "green" . "purple";
+
+if ($ball == 'red') {
+    $redbox = $ball;
+} else if ($ball == 'yellow') {
+    $yellowbox = $ball;
+} else if ($ball == 'blue') {
+    $bluebox == $ball;
+} else if ($ball == 'green') {
+    $greenbox = $ball;
+} else if ($ball == 'purple') {
+    $purplebox = $ball;
+} else {
+    $colorlessbox = $semua;
+}
+echo "red box : $redbox <br> ";
+echo "yellow box : <br>";
+echo "blue box : <br>";
+echo "green box : <br>";
+echo "purplebox :  <br>";
+echo "colorbox :$ball $semua  ";
