87690e27eecbc85979e06812d83b5d8799fa77ff
KRISTIANUS <61706410+PUTRAFLOBAMORA98@users.noreply.github.com>
2020-04-09 21:17:47 +0700

    07latihan8

---
+++ 07latihan8.php
@@ -0,0 +1,8 @@
+<HTML> <HEAD> <TITLE> Penggunaan Join </TITLE> 
+</HEAD>
+ <BODY>
+  <?php 
+  $var = array('18', '11', '2010');
+   $tanggal = join("/", $var); 
+   cho "$tanggal"; ?> 
+   </BODY> </HTML>
