08b1cad9ef8f9da8cec8a6b7f3b19a15814c2a10
KRISTIANUS <61706410+PUTRAFLOBAMORA98@users.noreply.github.com>
2020-04-09 20:50:33 +0700

    07latihan05

---
+++ 07latihan5.php
@@ -0,0 +1,25 @@
+<HTML> 
+<HEAD> 
+<TITLE> Penggunaan List </TITLE> 
+</HEAD> 
+<BODY> 
+<?php $program = array('Bobo','Doraemon','Spiderman'); list($Majalah, $Komik, $Film) = $program; echo "Jenis Buku & Hiburan :"; echo "<br />"; echo "Cerpen : $Majalah"; echo "<br />"; echo "Cerita Bergambar : $Komik"; echo "<br />"; echo "Bioskop : $Film"; 
+
+
+
+
+/*output
+Jenis Buku & Hiburan :
+Cerpen : Bobo
+Cerita Bergambar : Doraemon
+Bioskop : Spiderman */
+
+
+?> 
+</BODY> 
+
+</HTML>
+
+
+
+
